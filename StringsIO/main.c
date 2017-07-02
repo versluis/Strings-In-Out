f74c2531cf82c3fb64a84e69a9d469ec431fceb1
Jay Versluis <jay@versluis.com>
2017-07-02 10:55:14 -0400

    Initial Commit

---
+++ StringsIO/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StringsIO
+//
+//  Created by Jay Versluis on 02/07/2017.
+//  Copyright © 2017 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
